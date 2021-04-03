3f07769f22024e7e177d35175ebd47a3bafa6ffa
Provaldo <manos.sybo@gmail.com>
2021-04-03 21:16:51 +0300

    Added AuthenitcatedContent file

---
+++ Frontend-Backend/react-frontend/src/components/AuthenticatedContent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+const AuthenticatedContent = (props) => {
+  return <div> Authenticated Content </div>;
+};
+
+export default AuthenticatedContent;
